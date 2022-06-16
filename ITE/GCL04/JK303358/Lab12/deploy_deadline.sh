432dcfb627b2f323f6f58f05b1d8de380fbe7d0b
Jedrzej <jmpkxd@gmail.com>
2022-06-16 21:05:56 +0200

    Add: pictures

--- ITE/GCL04/JK303358/Lab12/deploy_deadline.sh
+++ ITE/GCL04/JK303358/Lab12/deploy_deadline.sh
@@ -1,8 +1,9 @@
 kubectl apply -f deployment.yml
 sleep 60
 kubectl rollout status -f deployment.yml
-if [[ "$?" -ne 0 ]] then
-	echo "deploy success"
-else
+if [[ "$?" -ne 0 ]]
+then
 	echo "deploy fail"
+else
+	echo "deploy success"
 fi
